6a5651bd955b7f06ffd212ddd6163de2ef95b6aa
johnkirtley <kirtleyj16@gmail.com>
2020-03-06 14:21:02 -0800

    Updated title

--- game-of-thrones/src/App.js
+++ game-of-thrones/src/App.js
@@ -5,7 +5,7 @@ import parse from 'html-react-parser';
 import { Route, Link } from 'react-router-dom';
 import { formatSeasons } from './utils/formatSeasons';
 import './App.css';
-import { Button, Grid } from '@material-ui/core'
+import { Button, Grid, Container } from '@material-ui/core'
 
 import { Episodes } from './components/Episodes';
 import { WatchedList } from './components/WatchedList';
@@ -42,7 +42,7 @@ export default function App() {
 
   return (
     <div className="App">
-      <h1><img className="title" src="https://fontmeme.com/permalink/200306/272f5270876666e7e43302429482f942.png" alt="game-of-thrones-font" border="0"></img></h1>
+      <h1><img className="title" src="https://fontmeme.com/permalink/200306/bde143405de2a86298ff02a29c36c19b.png" alt="game-of-thrones-font" border="0"></img></h1>
       {/* <Link to="/watched">Watched List</Link> */}
       {/* <Link to="/">Home</Link> */}
       {/* <label htmlFor="progress">Your Progress</label> */}
@@ -74,10 +74,10 @@ export default function App() {
             <p>Where To Watch: {show.network.name}</p>
           </div> */}
 
-          <div>
+          <Container maxWidth="sm">
             <h3>Summary</h3>
             <p>{parse(show.summary)}</p>
-          </div>
+          </Container>
           <Dropdown
             options={Object.keys(seasons)}
             onChange={handleChange}
