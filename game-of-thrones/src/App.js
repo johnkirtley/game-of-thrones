4e786d6d2153d06a143b430fe845ad8b59b935f9
johnkirtley <kirtleyj16@gmail.com>
2020-03-06 15:11:03 -0800

    Finished styling and optimized accessibility

--- game-of-thrones/src/App.js
+++ game-of-thrones/src/App.js
@@ -41,59 +41,63 @@ export default function App() {
   console.log(episodes)
 
   return (
-    <div className="App">
-      <h1><img className="title" src="https://fontmeme.com/permalink/200306/bde143405de2a86298ff02a29c36c19b.png" alt="game-of-thrones-font" border="0"></img></h1>
-      {/* <Link to="/watched">Watched List</Link> */}
-      {/* <Link to="/">Home</Link> */}
-      {/* <label htmlFor="progress">Your Progress</label> */}
-      {/* Will break progress bar out into own component once save
+    <main role="main">
+      <div className="App">
+        <h1><img className="title" src="https://fontmeme.com/permalink/200306/bde143405de2a86298ff02a29c36c19b.png" alt="game-of-thrones-font" border="0"></img></h1>
+        {/* <Link to="/watched">Watched List</Link> */}
+        {/* <Link to="/">Home</Link> */}
+        {/* <label htmlFor="progress">Your Progress</label> */}
+        {/* Will break progress bar out into own component once save
       functionality is complete */}
-      {/* <progress value={size} max={show._embedded.episodes.length}>15%</progress> */}
-      <Grid className="sub-info">
-        <Grid item>
-          <Button className="status" variant="outlined">Show Status: {show.status}</Button>
-        </Grid>
-        <Grid item>
-          <Button className="rating" variant="outlined">Average Rating: {show.rating.average}</Button>
-        </Grid>
-      </Grid>
-      <img className="main-image" src={show.image.original} alt={show.name} />
-
-      <div>
-        <Route exact path="/">
-          <Grid container direction="row" justify="center" className="genre-list" spacing={3}>
-            {show.genres.map(genre => (
-              <Grid item>
-                <Button className="genre" variant="outlined">{genre}</Button>
-              </Grid>
-            ))}
+        {/* <progress value={size} max={show._embedded.episodes.length}>15%</progress> */}
+        <Grid className="sub-info">
+          <Grid item>
+            <Button className="status" variant="outlined">Show Status: {show.status}</Button>
           </Grid>
+          <Grid item>
+            <Button className="rating" variant="outlined">Average Rating: {show.rating.average}</Button>
+          </Grid>
+        </Grid>
+        <img className="main-image" src={show.image.original} alt={show.name} />
 
-          {/* 
+        <div>
+          <Route exact path="/">
+            <Grid container direction="row" justify="center" id="genre-list" spacing={3}>
+              {show.genres.map(genre => (
+                <Grid item>
+                  <Button className="genre" variant="outlined">{genre}</Button>
+                </Grid>
+              ))}
+            </Grid>
+
+            {/* 
           <div>
             <p>Where To Watch: {show.network.name}</p>
           </div> */}
+            <Button variant="contained" id="dropdown">
+              <Dropdown
+                options={Object.keys(seasons)}
+                onChange={handleChange}
+                placeholder="Select a season"
+                value={selectedSeason || "Select a season"}
+              />
+            </Button>
 
-          <Container maxWidth="sm">
+            {/* <Container className="summary">
             <h3>Summary</h3>
             <p>{parse(show.summary)}</p>
-          </Container>
-          <Dropdown
-            options={Object.keys(seasons)}
-            onChange={handleChange}
-            placeholder="Select a season"
-            value={selectedSeason || "Select a season"}
-          />
-        </Route>
-        <WatchedContext.Provider value={[watched, setWatched]}>
-          <Route exact path="/" render={props => {
-            return <Episodes {...props} episodes={episodes} />
-          }} />
-          <Route exact path="/watched" render={props => {
-            return <WatchedList {...props} episodes={episodes} />
-          }} />
-        </WatchedContext.Provider>
+          </Container> */}
+          </Route>
+          <WatchedContext.Provider value={[watched, setWatched]}>
+            <Route exact path="/" render={props => {
+              return <Episodes {...props} episodes={episodes} />
+            }} />
+            <Route exact path="/watched" render={props => {
+              return <WatchedList {...props} episodes={episodes} />
+            }} />
+          </WatchedContext.Provider>
+        </div>
       </div>
-    </div>
+    </main>
   );
 }
--- game-of-thrones/src/components/Episodes.js
+++ game-of-thrones/src/components/Episodes.js
@@ -1,7 +1,7 @@
 import React, { useState, useContext } from 'react'
 import parse from 'html-react-parser'
 import WatchedContext from '../contexts/WatchedContext';
-import { Button, Grid, Paper, makeStyles } from '@material-ui/core';
+import { Button, Grid, Paper, Container } from '@material-ui/core';
 
 
 const initialState = {}
@@ -25,20 +25,22 @@ export const Episodes = ({ episodes }) => {
     console.log("episodes", episodes)
 
     return (
-        <Grid container justify="center" alignItems="center" spacing={3}>
-            {episodes.map(episode => {
-                return (
-                    <Grid item xs={6}>
-                        <Paper key={episode.id} className="episode-card">
-                            <h3>{episode.name}</h3>
-                            <img src={episode.image.original} alt={episode.name} className="episode-image" />
-                            <div>{parse(episode.summary)}</div>
-                            {/* <button onClick={addWatched} value={episode.name}>Watched</button> */}
-                        </Paper>
-                    </Grid>
-                )
-            })}
+        <Container>
+            <Grid container justify="center" alignItems="center" spacing={3}>
+                {episodes.map(episode => {
+                    return (
+                        <Grid item xs={6}>
+                            <Paper key={episode.id} id="episode-card">
+                                <h2 className="episode-name">{episode.name}</h2>
+                                <img src={episode.image.original} alt={episode.name} className="episode-image" />
+                                <div className="episode-summary">{parse(episode.summary)}</div>
+                                {/* <button onClick={addWatched} value={episode.name}>Watched</button> */}
+                            </Paper>
+                        </Grid>
+                    )
+                })}
 
-        </Grid>
+            </Grid>
+        </Container>
     )
 }
