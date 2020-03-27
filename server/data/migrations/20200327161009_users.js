16f9f42e3021d12dcc569239cffc7c08e40aad6c
johnkirtley <kirtleyj16@gmail.com>
2020-03-27 16:22:56 -0700

    :card_file_box: Added database schema
    Added initial database schema to hold username, password, and watched list

---
+++ server/data/migrations/20200327161009_users.js
@@ -0,0 +1,16 @@
+exports.up = function(knex) {
+	return knex.schema.createTable('users', tbl => {
+		tbl.increments();
+
+		tbl
+			.string('username')
+			.unique()
+			.notNullable();
+		tbl.string('password').notNullable();
+		tbl.string('watched');
+	});
+};
+
+exports.down = function(knex) {
+	return knex.schema.dropTableIfExists('users');
+};
