d912ce077397e2b80d934eeea76e4b7d5d97c224
johnkirtley <kirtleyj16@gmail.com>
2020-03-31 16:52:31 -0700

    :lipstick: Cleaned Up Styling
    Cleaned up certain styles

--- game-of-thrones/src/components/Progress.js
+++ game-of-thrones/src/components/Progress.js
@@ -10,6 +10,8 @@ export const Progress = props => {
 	const [maxEpisodes, setMaxEpisodes] = useState();
 	const [isLoading, setIsLoading] = useState(false);
 
+	console.log(maxEpisodes);
+
 	useEffect(() => {
 		fetchShow().then(res => {
 			setMaxEpisodes(res.data._embedded.episodes.length);
@@ -44,18 +46,14 @@ export const Progress = props => {
 			) : (
 				''
 			)}
-			<p>{Math.floor((watched.length / maxEpisodes) * 100)}% Complete</p>
+			<p>{Math.round((watched.length / maxEpisodes) * 100)}% Complete</p>
 			<Line
 				className='progress-bar'
-				percent={watched.length}
 				strokeWidth='4'
 				strokeColor='green'
 				trailWidth='2'
 				percent={(watched.length / maxEpisodes) * 100}
 			/>
-			{/* <progress value={watched.length} max={maxEpisodes}>
-				15%
-			</progress> */}
 			{watched.map(episode => (
 				<Paper elevation={2} variant='outlined' className='watched-episode'>
 					{episode.episode_name}
