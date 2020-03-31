b4d9cd78bb01898da4edda5bf9baff21a8cb21e9
johnkirtley <kirtleyj16@gmail.com>
2020-03-31 16:47:25 -0700

    :lock: Added Form Validation
    Added form validation for registration

--- game-of-thrones/src/auth/Register.js
+++ game-of-thrones/src/auth/Register.js
@@ -1,5 +1,8 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
 import axios from 'axios';
+
+import { TextField, Button } from '@material-ui/core';
 
 const initialState = {
 	username: '',
@@ -8,42 +11,89 @@ const initialState = {
 
 export const Register = props => {
 	const [credentials, setCredentials] = useState(initialState);
+	const [confirmPass, setConfirmPass] = useState('');
 
-	const handleChanges = e => {
+	const handleChanges = async e => {
 		setCredentials({
 			...credentials,
 			[e.target.name]: e.target.value
 		});
+
 		console.log(credentials);
 	};
 
+	const handleConfirm = e => {
+		setConfirmPass({
+			...confirmPass,
+			[e.target.name]: e.target.value
+		});
+	};
+
+	useEffect(() => {
+		console.log(confirmPass);
+	}, [confirmPass]);
+
 	const handleSubmit = async e => {
 		e.preventDefault();
-		await axios
-			.post(
-				'https://game-of-thrones-backend.herokuapp.com/api/auth/register',
-				credentials
-			)
-			.then(res => {
-				props.history.push('/login');
-			})
-			.catch(err => {
-				console.log('Error registering', err);
-			});
+
+		const password = credentials.password;
+		const confirm = confirmPass.confirmPass;
+
+		if (password === confirm) {
+			await axios
+				.post(
+					'https://game-of-thrones-backend.herokuapp.com/api/auth/register',
+					credentials
+				)
+				.then(res => {
+					props.history.push('/login');
+				})
+				.catch(err => {
+					console.log('Error registering', err);
+				});
+		} else {
+			alert('Passwords dont match');
+		}
 	};
 
 	return (
-		<div>
-			<form onSubmit={handleSubmit}>
-				<label htmlFor='username'>Username</label>
-				<input type='text' name='username' onChange={handleChanges} />
-
-				<label htmlFor='password'>Password</label>
-				<input type='text' name='password' onChange={handleChanges} />
-
-				<label htmlFor='confirmPass'>Confirm Password</label>
-				<input type='text' name='confirmPass' />
-				<button>Register</button>
+		<div className='form-container'>
+			<form onSubmit={handleSubmit} autoComplete='off'>
+				<div>
+					<TextField
+						htmlFor='username'
+						label='Username'
+						name='username'
+						onChange={handleChanges}
+						required
+					/>
+				</div>
+				<div>
+					<TextField
+						htmlFor='password'
+						label='Password'
+						type='password'
+						name='password'
+						onChange={handleChanges}
+						required
+					/>
+				</div>
+				<div>
+					<TextField
+						htmlFor='confirmPass'
+						label='Confirm Password'
+						type='password'
+						name='confirmPass'
+						onChange={handleConfirm}
+						required
+					/>
+				</div>
+				<Button id='register-button' variant='contained' type='submit'>
+					Register
+				</Button>
+				<div>
+					<Link to='/login'>Have An Account Already?</Link>
+				</div>
 			</form>
 		</div>
 	);
