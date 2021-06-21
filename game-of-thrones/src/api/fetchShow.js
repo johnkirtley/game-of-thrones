0f64b47358f9c2a643f4decb4ecbcf121db5ba93
johnkirtley <kirtleyj16@gmail.com>
2021-06-21 12:19:54 -0700

    Updated API Endpoint

--- game-of-thrones/src/api/fetchShow.js
+++ game-of-thrones/src/api/fetchShow.js
@@ -1,17 +1,16 @@
 import axios from 'axios';
 
-
 export const fetchShow = () => {
-    return axios
-        .get(
-            "https://api.tvmaze.com/singlesearch/shows?q=game-of-thrones&embed=episodes"
-        )
-        .then(res => {
-            console.log(res)
-            return res;
-        })
-        .catch(err => {
-            console.log('Fetching API', err);
-            return err
-        })
+	return axios
+		.get(
+			'https://api.tvmaze.com/singlesearch/shows?q=game%20of%20thrones&embed=episodes'
+		)
+		.then((res) => {
+			console.log(res);
+			return res;
+		})
+		.catch((err) => {
+			console.log('Fetching API', err);
+			return err;
+		});
 };
